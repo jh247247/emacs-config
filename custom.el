06b43e437f620f27af93a74f2d4768cbb31d0dae
Jack Hosemans <jh247247@gmail.com>
2013-08-05 21:22:50 +1000

    Fix latex command

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-command "pdflatex")
  '(hes-simple-modes
    (quote
     (js-mode js2-mode python-mode c-mode c++-mode java-mode)))
