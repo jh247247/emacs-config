222af7c066184fdcf623ea5f2d743f1e5f31813d
Jack Hosemans <jh247247@gmail.com>
2013-08-22 21:31:50 +1000

    Better integrate god-mode with this config. Set menu key to be the enable.

--- setup-universal.el
+++ setup-universal.el
@@ -118,10 +118,12 @@ Symbols matching the text at point are put first in the completion list."
 
 (defun my-update-cursor ()
   (setq cursor-type (if (or god-local-mode buffer-read-only)
-                        'box
-                      'bar)))
+                        'bar
+                      'box)))
 
 (add-hook 'god-mode-enabled-hook 'my-update-cursor)
 (add-hook 'god-mode-disabled-hook 'my-update-cursor)
 
+(global-unset-key (kbd "<menu>"))
+(global-set-key (kbd "<menu>") 'god-local-mode)
 (provide 'setup-universal)
