c089b1466e7e06873c10e9275b8fbe94b304dff9
Jack Hosemans <jh247247@gmail.com>
2014-06-09 17:49:06 +1000

    Fleshed out autoparentheses matching with autopair. Possibly some other things I forgot.

--- init.el
+++ init.el
@@ -19,3 +19,15 @@
   (require 'ob))
 
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "emacs.org$"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-diff-options nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
