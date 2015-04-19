faa2f516a2b8bfc526d51b4afb4d1ba27107d5a7
Jack Hosemans <jh247247@gmail.com>
2015-04-19 12:11:58 +1000

    Some general cleanup and maintenance.

--- init.el
+++ init.el
@@ -24,7 +24,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(magit-diff-options nil)
  '(persp-auto-save-opt 0))
 (custom-set-faces
