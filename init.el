250d02b1de4f4c8b5bc748d8910e84f96fda32cb
Jack Hosemans <jh247247@gmail.com>
2015-06-26 21:30:55 +1000

    Rejiggered semantic mode to use the git repo for android stuff, configured it for android programming

--- init.el
+++ init.el
@@ -24,6 +24,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cedet-android-current-version "22")
+ '(cedet-android-sdk-root "/opt/android/sdk")
  '(custom-safe-themes
    (quote
     ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
